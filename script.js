05b05136f665b04324ed3e83fb432f372ff66020
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-03-12 01:29:41 -0800

    cleaning up code

--- script.js
+++ script.js
@@ -20,12 +20,12 @@ generateBtn.addEventListener("click", writePassword);
   var randomNumber = ["1","2","3","4","5","6","7","8","9","0"];
   var lowerCase = [ "a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var symbol = ["!","@","#", "$","%","^","&","*","(",")","-","+","=","?",">","<"];
+  var symbol = ["!","@","#", "$","%","^","&","*","(",")","-","+","=","?",">","<","}","{","[","]","|"];
 
 function generatePassword () {
 //get user preferences
 
-   let Chooselength = prompt ("how many charecters do you want in your password?", "between 8 and 128");
+   let Chooselength = prompt ("How many charecters do you want in your password?", "Choose a number between 8 and 128");
    let ChooseLowerCase = confirm ("Do you want lower case letters?");
    let ChooseUpperCase = confirm ("Do you want upper case letters?");
    let Numbers = confirm ("Do you want numbers?");
@@ -36,59 +36,55 @@ function generatePassword () {
 
 
 
-  //setting password length
-
+  //setting users password length
   if (Chooselength < 8) {
     window.alert ("please choose a number higher than 8");
     return;
   }if (Chooselength > 128) {
     window.alert ("Invalid response! Please choose a password length between 8 and 128!");
     return;
-  }
+  };
  
-  //identify and collect all preferences into one variable
+  
+  //identifying and collecting all user preferences into one variable
 
- // choosing lower case
-
+  // choosing lower case
   if (ChooseLowerCase === true) {
     charecter = lowerCase.concat (charecter);
-  }
+  };
 
   //choosing uppercase
   if (ChooseUpperCase === true) {
     charecter = upperCase.concat(charecter);
-  }
+  };
 
   //choosing numbers
   if (Numbers === true) {
     charecter = randomNumber.concat(charecter);
-  }
+  };
   
   //choosing symbols
   if (Symbols === true) {
     charecter = symbol.concat(charecter);
-  }
+  };
 
 
   //randomly draw a charecter, loop through for the specified length
     for (var i = 0; i < Chooselength; i++) {
       var randomItem= charecter[Math.floor(Math.random() * charecter.length)];
 
-    //put charecter on the password array
+    //put selected charecter on the password array
       password.push (randomItem);
-    }
-  
-  // 3. create a garunteed collection
- 
-  // 8. loops through garunteed elememts, replace elements in password array
+    };
 
-  // join charecters in the password array into a string
+
+  // joining charecters in the password array into a string
   let finalPassword = password.join("")
 
-  // return the password string
- return finalPassword;
+  // return the password as a string
+  return finalPassword;
  
-}
+};
 
  
 
